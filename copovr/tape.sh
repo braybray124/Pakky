ad09f7b3175a3a20d4f09bd912fbc046b7b75c4b
penguin <bradenclancy408@gmail.com>
2026-02-07 03:39:47 -0500

    modified:   copovr/tape.sh

--- copovr/tape.sh
+++ copovr/tape.sh
@@ -3,8 +3,12 @@
 CONFIG_FILE="/etc/pakky.conf"
 if [[ -f "$CONFIG_FILE"]]; then
   source "CONFIG_FILE"
+ then 
+  echo "Configuration loaded from $CONFIG_FILE"
+  then wget -O /etc/pakky/pakky.conf "$CONFIG_FILE"
+  source "/etc/pakky/pakky.conf"
 fi 
 
 sync_ports() {
-    echo "Syncing the 
+    echo "Updating tree
 }
