ccbf9c8412961bc9403fd655392de42296cd35b2
penguin <bradenclancy408@gmail.com>
2026-02-09 16:06:09 -0500

    modified:   copovr/pakky.conf 	modified:   copovr/tape.sh

--- copovr/tape.sh
+++ copovr/tape.sh
@@ -1,14 +1,25 @@
-#/!bin/bash
+#!/bin/bash
 
-CONFIG_FILE="/etc/pakky.conf"
-if [[ -f "$CONFIG_FILE"]]; then
-  source "CONFIG_FILE"
- then 
-  echo "Configuration loaded from $CONFIG_FILE"
-  then wget -O /etc/pakky/pakky.conf "$CONFIG_FILE"
-  source "/etc/pakky/pakky.conf"
-fi 
+GLOBAL_CONFIG="/etc/pakky/pakky.conf"
+LOCAL_CONFIG="./pakky.conf"
+
+if [[-f $GLOBAL_CONFIG ]]; then
+    source $GLOBAL_CONFIG
+    echo "Loaded global config from $GLOBAL_CONFIG"
+elif [[-f $LOCAL_CONFIG ]]; then
+    source $LOCAL_CONFIG
+    echo "Loaded local config from $LOCAL_CONFIG"
+else
+    echo "No config file found, using defaults"
+else 
+    echo "Error loading config file"
+    exit 1
+fi
 
 sync_ports() {
-    echo "Updating tree
+    echo "Syncing ports from $PAKKY_MIRROR"
+    git clone $PAKKY_MIRROR /var/lib/pakky/ports
 }
+
+build_package() {
+    echo "Building package 
